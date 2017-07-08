31c25e3ceea0b2f8204859bf9f365462306b6411
Andy Bonner <abonner@duke.edu>
2017-07-08 13:41:46 -0400

    create authorization error handling function

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,3 +12,29 @@
    e.preventDefault();
    $("#login-modal").modal('open');
  });
+
+ $("#profile-input").submit(function (e) { 
+  e.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(handleAuthError);
+ });
+
+ function handleAuthError(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === "auth/user-not-found") {
+    Materialize.toast(`
+      <h3>Who?</h3>
+      <p>We don't recognize that combo of email and password.</p>
+      <p>Want to create a <a class="waves-effect btn" onclick="createNewUser()">New user</a> ?</p>
+      <p>Or just <a class="waves-effect btn">try again</a> ?
+    `);
+  } else {
+  var errorToastTxt = `
+    <h3>I'm sorry, there's been a problem!</h3>
+    <p>Error code "${errorCode}": ${errorMessage}.</p>
+    <a class="btn waves-effect">OK</a>
+  `;
+  Materialize.toast(errorToastTxt);
+  }
+}
