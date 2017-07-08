6cc8f682815b6c2b640a762b93587dbe299b2032
Michael Dinh <mkdinh94@gmail.com>
2017-07-08 11:04:33 -0400

    add login page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,3 +3,7 @@
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   $('.collapsible').collapsible();
 
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
