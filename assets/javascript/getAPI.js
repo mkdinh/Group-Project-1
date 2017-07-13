fc7a8af1fc29578883617555fbcc911cc83e37a6
Michael Dinh <mkdinh94@gmail.com>
2017-07-13 11:38:56 -0400

    fixing day-temp font-size media query

--- assets/javascript/getAPI.js
+++ assets/javascript/getAPI.js
@@ -713,13 +713,13 @@ function updateClock() {
 // Html page interactions js 
 $(document).ready(function(){
 
-	$(window).resize(function(){
-		if ($(window).width() < 480) {
-	    	$('body').css('font-size','80%');
-		} else {
-		    $('body').css('font-size','100%');
-		}
-	})
+	// $(window).resize(function(){
+	// 	if ($(window).width() < 480) {
+	//     	$('body').css('font-size','80%');
+	// 	} else {
+	// 	    $('body').css('font-size','100%');
+	// 	}
+	// })
 
 	// update clock every 1 second
 	setInterval(updateClock, 1000);
